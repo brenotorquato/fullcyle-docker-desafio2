8e673d0bd51b5780b2656cf8b011d53f8df026ca
Breno Torquato <brenotorquatoes@gmail.com>
2024-02-06 20:57:59 -0300

    Configurada rede de acesso entre servidores

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express()
 const SERVER_PORT = process.env.SERVER_PORT
 
 app.get('/', (req, res) => {
-    res.send('<h1>Full Cycle!!!!</h1>')
+    res.send('<h1>Full Cycle - COM REDE CONFIGURADA!!!!</h1>')
 });
 
 app.listen(SERVER_PORT, () => {
